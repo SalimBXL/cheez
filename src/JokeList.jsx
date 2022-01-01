83e7e4c8b28873db96861deb6bf82b5a7def762f
Salim A. JOLY <el.salim.salim@gmail.com>
2022-01-01 23:28:15 +0100

    fini

--- src/JokeList.jsx
+++ src/JokeList.jsx
@@ -11,6 +11,8 @@ const JokeList = ({numJokesToGet}) => {
     const [jokes, setJokes] = useState(
             JSON.parse(window.localStorage.getItem("jokes") || "[]")
         );
+    const [loading, setLoading] = useState(false);
+    const seenJokes = new Set(jokes.map(joke => joke.joke));
 
     useEffect(() => {
         (jokes.length === 0) && fetchJokes();
@@ -21,19 +23,26 @@ const JokeList = ({numJokesToGet}) => {
     }
 
     async function fetchJokes() {
-        const _jokes = [];
-        while(_jokes.length < numJokesToGet) {
-            let response = await axios.get(API_URL, {headers: {"Accept": "application/json"}});
-            let data = await response.data;
-            (data.status === 200) && _jokes.push(
-                { id: uuid(), joke: data.joke, vote: 0}
-            );
+        setLoading(true);
+        try {
+            const _jokes = [];
+            while(_jokes.length < numJokesToGet) {
+                let response = await axios.get(API_URL, {headers: {"Accept": "application/json"}});
+                let data = await response.data;
+                const joke = data.joke;
+                (!seenJokes.has(joke)) && _jokes.push(
+                    { id: uuid(), joke: data.joke, vote: 0}
+                );
+            }
+            setJokes(prevs => {
+                const news = [...prevs, ..._jokes];
+                window.localStorage.setItem("jokes", JSON.stringify(news));
+                return news;
+            });
+        } catch(e) {
+            alert(e);
         }
-        setJokes(prevs => {
-            const news = [...prevs, ..._jokes];
-            window.localStorage.setItem("jokes", JSON.stringify(news));
-            return news;
-        });
+        setLoading(false);
     }
 
     const handleVote = (id, delta) => {
@@ -48,33 +57,40 @@ const JokeList = ({numJokesToGet}) => {
             return news;
         });
     }
-    
-    const jokeList = jokes.map((joke) => 
+
+    const loadingScreen = () => (
+        <div className="JokeList-spinner">
+            <i className="far fa-8x fa-laugh fa-spin"></i>
+            <h1 className="JokeList-title">Loading...</h1>
+        </div>);
+
+    const jokeList = jokes.sort((a,b) => b.vote - a.vote).map((joke, idx) => (
         <Joke   key={joke.id} 
                 id={joke.id} 
-                joke={joke.joke} 
+                joke={(idx+1) + "/" + jokes.length + " - " + joke.joke}
                 vote={joke.vote}
                 upVote={() => handleVote(joke.id, 1)}
                 downVote={() => handleVote(joke.id, -1)}
-        />);
+        />));
 
-    return (
+    const jokeListScreen = () => (
         <div className="JokeList">
             <div className="JokeList-sidebar">
                 <h1 className="JokeList-title">
                     <span>Dad</span> Jokes
                 </h1>
                 <h2 className="JokeList-subtitle">React Random Dad's Joke Demo</h2>
-                <img src={logo} alt="logo"></img>
+                <img className="JokeList-logo" src={logo} alt="logo"></img>
                 <button className="JokeList-button" onClick={handleClick}>New Jokes</button>
             </div>
-            
-            
             <div className="JokeList-jokes">
                 {jokeList}
             </div>
-        </div>
-    );
+        </div>);
+
+    return (loading) 
+        ? loadingScreen()
+        : jokeListScreen();
 }
 
 JokeList.defaultProps = {
